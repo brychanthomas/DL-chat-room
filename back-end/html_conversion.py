0f22670735f3875833d74e8963ef82ff0f89551b
Brychan Thomas <41427164+brychanthomas@users.noreply.github.com>
2020-06-18 13:14:00 +0100

    Added unix time conversion

--- back-end/html_conversion.py
+++ back-end/html_conversion.py
@@ -1,10 +1,12 @@
+from datetime import datetime
+
 #Converter Function
 def conversion (tup1e):
     string=""
     for pengs in range (len(tup1e)):
         name=(tup1e[pengs][0])
         msg=(tup1e[pengs][1])
-        time=(tup1e[pengs][2]) 
+        time=datetime.utcfromtimestamp(tup1e[pengs][2]).strftime('%Y/%m/%d %H:%M:%S')
         string+=("\n<p>"+name+" at "+time+"</p>"+"\n<p>"+msg+"</p>"+"\n<br>")
     return string
 
